0a68d148dca6b9dd455d16de688a72f516943fe7
rcarrascalc <r.carrascalc@uniandes.edu.co>
2025-02-19 07:32:32 -0500

    dejar valores duplicados en ordenes

--- apps/order-service/app/models.py
+++ apps/order-service/app/models.py
@@ -3,4 +3,4 @@ from app import db
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(100), nullable=False)
-    product = db.Column(db.String(100), unique=True, nullable=False)
+    product = db.Column(db.String(100), nullable=False)
