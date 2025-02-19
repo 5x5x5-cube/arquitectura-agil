edbe75bf943f055ef75d85e32cfc85de506d482a
fabriziocucina-uniandes <f.cucina@uniandes.edu.co>
2025-02-19 16:53:56 -0500

    added blueprints to app in order service

--- apps/order-service/app.py
+++ apps/order-service/app.py
@@ -1,7 +1,9 @@
 from app import create_app
+from app.routes import api_bp
 
 app = create_app()
+app.register_blueprint(api_bp)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(debug=True, port=5000)
